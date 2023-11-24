e14ed64d9cff8350ba3af35fe03047f4417f0aa8
Aleksey Bystrov <45369628+AlxBystrov@users.noreply.github.com>
2023-11-24 08:42:38 +0300

    chore: hw05 is done (#6)
    * chore: hw05 is done
    ---------
    
    Co-authored-by: Aleksey Bystrov <a.bystrov@crpt.ru>

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,56 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded   = errors.New("errors limit exceeded")
+	ErrTooLowGoroutineNumber = errors.New("at least two goroutine must be specified for concurrently execution")
+)
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n < 2 {
+		return ErrTooLowGoroutineNumber
+	}
+	var errCount int32
+	chTask := make(chan Task)
+	wg := sync.WaitGroup{}
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go executor(&errCount, chTask, &wg)
+	}
+
+	for _, task := range tasks {
+		if int(atomic.LoadInt32(&errCount)) >= m && m > 0 {
+			// stop iteration on tasks cause of errors
+			break
+		}
+		chTask <- task
+	}
+
+	close(chTask)
+	wg.Wait()
+
+	// ignore all errors, when m <= 0
+	if int(atomic.LoadInt32(&errCount)) >= m && m > 0 {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func executor(errCount *int32, chTask <-chan Task, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range chTask {
+		err := task()
+		if err != nil {
+			// increase errors
+			atomic.AddInt32(errCount, 1)
+		}
+	}
 }
