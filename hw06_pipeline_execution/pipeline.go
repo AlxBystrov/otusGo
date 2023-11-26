71418c92ad8cf343700e3847b792ce48fe95348f
Aleksey Bystrov <a.bystrov@crpt.ru>
2023-11-26 09:30:53 +0300

    chore: hw06 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,50 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	resultStream := make(chan interface{})
+	finished := make(chan struct{})
+	go func() {
+		defer close(resultStream)
+		for {
+			select {
+			case <-done:
+				return
+			case <-finished:
+				return
+			}
+		}
+	}()
+	go func() {
+		var dataStream In
+		for i, stage := range stages {
+			switch i {
+			case 0:
+				dataStream = stageExec(done, in, stage)
+			case len(stages) - 1:
+				for val := range stageExec(done, dataStream, stage) {
+					resultStream <- val
+				}
+				finished <- struct{}{}
+			default:
+				dataStream = stageExec(done, dataStream, stage)
+			}
+		}
+	}()
+	return resultStream
+}
+
+func stageExec(done In, in In, stage Stage) Out {
+	stageStream := make(chan interface{})
+	go func() {
+		defer close(stageStream)
+		for val := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+				stageStream <- val
+			}
+		}
+	}()
+	return stageStream
 }
