a076f15b5df6a48158b2b7c773c1c8ad9903b408
Aleksey Bystrov <a.bystrov@crpt.ru>
2023-11-29 15:52:07 +0300

    chore: hw06 another version

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,50 +9,45 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	resultStream := make(chan interface{})
-	finished := make(chan struct{})
+	resultStream := make(Bi)
+	gen := make(Bi)
+
+	// gen -> to be able to stop immediately
+	go func() {
+		defer close(gen)
+		for val := range in {
+			select {
+			case <-done:
+				return
+			case gen <- val:
+			}
+		}
+	}()
+
+	dataStream := make(In)
+	for i, stage := range stages {
+		switch i {
+		case 0:
+			// use gen for first stage
+			dataStream = stage(gen)
+		default:
+			dataStream = stage(dataStream)
+		}
+	}
+
 	go func() {
 		defer close(resultStream)
 		for {
 			select {
 			case <-done:
 				return
-			case <-finished:
-				return
-			}
-		}
-	}()
-	go func() {
-		var dataStream In
-		for i, stage := range stages {
-			switch i {
-			case 0:
-				dataStream = stageExec(done, in, stage)
-			case len(stages) - 1:
-				for val := range stageExec(done, dataStream, stage) {
-					resultStream <- val
+			case val, ok := <-dataStream:
+				if !ok {
+					return
 				}
-				finished <- struct{}{}
-			default:
-				dataStream = stageExec(done, dataStream, stage)
+				resultStream <- val
 			}
 		}
 	}()
 	return resultStream
-}
-
-func stageExec(done In, in In, stage Stage) Out {
-	stageStream := make(chan interface{})
-	go func() {
-		defer close(stageStream)
-		for val := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-				stageStream <- val
-			}
-		}
-	}()
-	return stageStream
 }
