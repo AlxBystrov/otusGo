2d58f3d077ebf691cdc4396adb9531af406253d4
Aleksey Bystrov <45369628+AlxBystrov@users.noreply.github.com>
2023-12-12 06:47:36 +0300

    chore: hw06 done (#7)
    * chore: hw06 done
    ---------
    
    Co-authored-by: Aleksey Bystrov <a.bystrov@crpt.ru>

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,45 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	resultStream := make(Bi)
+	gen := make(Bi)
+
+	// gen -> to be able to stop immediately
+	go func() {
+		defer close(gen)
+		for val := range in {
+			select {
+			case <-done:
+				return
+			case gen <- val:
+			}
+		}
+	}()
+
+	dataStream := make(In)
+	for i, stage := range stages {
+		switch i {
+		case 0:
+			// use gen for first stage
+			dataStream = stage(gen)
+		default:
+			dataStream = stage(dataStream)
+		}
+	}
+
+	go func() {
+		defer close(resultStream)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-dataStream:
+				if !ok {
+					return
+				}
+				resultStream <- val
+			}
+		}
+	}()
+	return resultStream
 }
